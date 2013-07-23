9c58f68efb0edc499d856a229054f3c971dde437
KarleXaw <karle.xaw@gmail.com>
2013-07-23 17:12:36 +0200

    First local commit

---
+++ Phonegap_Reservas/assets/www/js/gethoras.js
@@ -0,0 +1,36 @@
+var horas;
+var url;
+
+/*$('#horasPage').bind('pageinit', function(event) {
+	var fecha = "2012-12-13";
+	url = "http://kometa.pusku.com/form/gethoras.php" + "?fecha=" + fecha;
+	getHoras();
+});*/
+
+$('#formfecha').bind('change', function(event) {
+	var fecha = $("#formfecha").val();
+	url = "http://kometa.pusku.com/form/gethoras.php" + "?fecha=" + fecha;
+	getHoras();
+});
+
+$('#lista-horas').bind('change', function(event) {
+	var hora = $("#formfecha").val();
+	$.mobile.changePage ($("#reserva"));
+	$('#time_dt').val(hora);
+});
+
+function getHoras() {
+	$.getJSON(url, function(data) {
+		
+		$('#lista-horas option').remove();
+		horas = data.items;
+		$.each(horas, function(index, hora) {
+			$("#lista-horas").append('<option value=' + index +
+			'>' + hora.hora + 
+			'</option>');
+		});
+		
+		$("#lista-horas").trigger("change");
+		$("#lista-horas").selectmenu("open");
+	});
+}
