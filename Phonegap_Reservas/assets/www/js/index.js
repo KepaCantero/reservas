e9479f0d14376526a170008e0b3121a057622005
Kodek81 <contact.kodek81@gmail.com>
2013-08-27 12:22:05 -0700

    Añadida pantalla de selección para elegir menús o crear platos. Creado formulario para crear nuevos platos. El formulario incluye dos listas desplegables que se pueblan a partir de las correspondientes claves externas de la BD.

--- Phonegap_Reservas/assets/www/js/index.js
+++ Phonegap_Reservas/assets/www/js/index.js
@@ -1,11 +1,39 @@
 var urlMenuItems;
 var menuElegido;
+var urlCategorias = "http://kometa.pusku.com/form/getcategorias.php";
+var urlMenus = "http://kometa.pusku.com/form/getmenus.php";
 
 //EVENTOS AL CARGARSE LAS PÁGINAS
 
-/*$('#listaPage').bind('pageload', function(event) {
-	$("#listaItems").listview();
-});*/
+/* Estos preventDefault y unbind sirven para que los botones no propaguen
+ * sus clicks a los botones que salen debajo al cambiar de página. Hay
+ * que meter el bind en el pageshow para que el listener vuelva a crearse
+ * cada vez que se muestra el botón.
+ */
+$('#inicioPage').bind('pageshow', function(event) {
+	$('#botElegirMenus').bind('vclick', function(event) {
+		event.preventDefault(); 
+		event.stopImmediatePropagation(); 
+		$('#botElegirMenus').unbind('vclick');
+		$.mobile.changePage ($("#menuSelecPage"));
+	});
+	
+	$('#botCrearPlatos').bind('vclick', function(event) {
+		event.preventDefault(); 
+		event.stopImmediatePropagation(); 
+		$('#botCrearPlatos').unbind('vclick');
+		$.mobile.changePage ($("#crearPlatosPage"));
+	});
+});
+
+$('#crearPlatosPage').bind('pageshow', function(event) {
+	getCategorias();
+	getMenus();
+});
+
+$('#menuListaPage').bind('pageshow', function(event) {
+});
+
 
 
 //LISTENERS DE COMPONENTES 
@@ -13,19 +41,38 @@ var menuElegido;
 $('#botMenuDia').bind('vclick', function(event) {
 	menuElegido = 1;
 	getMenuItems();
-	$.mobile.changePage ($("#listaPage"));
+	$.mobile.changePage ($("#menuListaPage"));
 });
 
 $('#botMenuEspecial').bind('vclick', function(event) {
 	menuElegido = 2;
 	getMenuItems();
-	$.mobile.changePage ($("#listaPage"));
+	$.mobile.changePage ($("#menuListaPage"));
 });
 
 $('#botMenuCarta').bind('vclick', function(event) {
 	menuElegido = 3;
 	getMenuItems();
-	$.mobile.changePage ($("#listaPage"));
+	$.mobile.changePage ($("#menuListaPage"));
+});
+
+$('#formPlatos').submit(function() { 
+	var request = $.ajax({
+		url: 'http://kometa.pusku.com/form/insertplato.php',
+		type: 'POST',
+		data: { nombre: $("#inputNombrePlato").val(),
+		        descripcion: $("#inputDescPlato").val(),
+		        precio: $("#inputPrecioPlato").val(),
+		        categoria: $("#inputCategoriaPlato").val(),
+		        menu: $("#inputMenuPlato").val() },
+		success: function(obj){
+			alert("Plato añadido");
+			AddToCalendar();
+		},
+		error: function(error) {
+			alert(error);
+		}
+	});
 });
 
 
@@ -43,14 +90,45 @@ function getMenuItems() {
 		$.each(menuItems, function(index, menuItem) {
 			$("#listaItems").append(
 				"<li>" +
-			        /*"<a href=#>" +*/
+			        "<a href=#>" +
 			        "<img class='imagen' src='img/cordova.png'>" +
 			        "<h1 class='txtnombre'>" + menuItem.nombreItem + "</h1>" +
 			        "<p class='txtdesc'>" + menuItem.descItem + "</p>" +
 			        "<h2 class='txtprecio'>" + menuItem.precioItem + " €" + "</h2>" +
-			        /*"</a>" +*/
+			        "</a>" +
 		        "</li>");
 		});
 		$("#listaItems").listview("refresh");
+	});
+}
+
+function getCategorias() {
+	$.getJSON(urlCategorias, function(data) {
+		
+		$('#inputCategoriaPlato option').remove();
+		$("#inputCategoriaPlato").append('<option data-placeholder="true">Categoría</option>');
+		var categorias = data.items;		
+		$.each(categorias, function(index, categoria) {
+			$("#inputCategoriaPlato").append('<option value=' + categoria.categoriaID +
+			'>' + categoria.nombreCategoria + 
+			'</option>');
+		});
+		$("#inputCategoriaPlato").trigger("change");
+	});
+}
+
+
+function getMenus() {
+	$.getJSON(urlMenus, function(data) {
+		
+		$('#inputMenuPlato option').remove();
+		$("#inputMenuPlato").append('<option data-placeholder="true">Menú</option>');
+		var menus = data.items;		
+		$.each(menus, function(index, menu) {
+			$("#inputMenuPlato").append('<option value=' + menu.menuID +
+			'>' + menu.nombreMenu + 
+			'</option>');
+		});
+		$("#inputMenuPlato").trigger("change");
 	});
 }
